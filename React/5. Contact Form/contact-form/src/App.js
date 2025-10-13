d1ddc1e67ec5e1cc248157ed4551582c34929c18
sumantgigacars <sumant.shah@gigacars.com>
2025-10-14 03:54:36 +0530

    initial commit

---
+++ React/5. Contact Form/contact-form/src/App.js
@@ -0,0 +1,46 @@
+import './styles.css';
+
+function App() {
+  
+  const submitForm = (event) => {
+    event.preventDefault();
+    // Create a FormData object from the form
+    const formData = new FormData(event.target);
+
+    // Convert to a plain object (optional)
+    const data = Object.fromEntries(formData.entries());
+
+    console.log('Form Data:', data);
+  }
+
+
+  return (
+    <>
+      <form
+        onSubmit={submitForm}
+        // method='post'
+      >
+        <div>
+          <label htmlFor='name-input'>Name</label>
+          <input id="name-input" name='name' type='text' />
+        </div>
+        <div>
+          <label htmlFor='email-input'>Email</label>
+          <input id="email-input" name="email" type='email' />
+        </div>
+        <div>
+          <label htmlFor='message-input'>Message</label>
+          <textarea 
+            id='message-input'
+            name='message'
+          ></textarea>
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
